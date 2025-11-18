08aed3adab96d935c202dfab0ea85003f6e784c5
Deepak-136 <deepakn132006@gmail.com>
2025-11-18 20:13:29 +0530

    Navbar Completed

---
+++ js/main.js
@@ -0,0 +1,95 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const sections = [
+    "home",
+    "how-it-works",
+    "features",
+    "about",
+    "faq",
+    "blog",
+    "contact"
+  ];
+ let loadedCount = 0;
+ const total = sections.length;
+
+  sections.forEach(async (section) => {
+    try {
+      const res = await fetch(`${section}.html`);
+      if (!res.ok) throw new Error(`${section}.html not fetched`);
+
+      const html = await res.text();
+      document.getElementById(section).innerHTML = html;
+      console.log(`${section}.html loaded `);
+    } catch (err) {
+      console.error(err);
+      document.getElementById(section).innerHTML =
+        "<p>Page not Found</p>";
+    }finally{
+        loadedCount++;
+        if(loadedCount === total) hideLoader();
+    }
+  });
+
+  function hideLoader(){
+    const loader = document.getElementById("loader");
+    loader.style.opacity=0;
+    setTimeout(()=>{
+        loader.style.display="none";
+        document.body.classList.add("loaded")
+    })
+  }
+
+  const links = document.querySelectorAll('nav a[href^="#"]');
+  links.forEach((link) => {
+    link.addEventListener("click", (e) => {
+      e.preventDefault();
+      const targetId = link.getAttribute("href");
+      const target = document.querySelector(targetId);
+      if (target) {
+        target.scrollIntoView({ behavior: "smooth" });
+      }
+    });
+  });
+
+  const navLinks = document.querySelectorAll("nav a");
+
+  window.addEventListener("scroll", () => {
+    let current = "";
+
+    sections.forEach((id) => {
+      const sectionEl = document.getElementById(id);
+      if (!sectionEl) return;
+      const sectionTop = sectionEl.offsetTop;
+      const sectionHeight = sectionEl.offsetHeight;
+
+      if (pageYOffset >= sectionTop - sectionHeight / 3) {
+        current = id;
+      }
+    });
+
+    navLinks.forEach((a) => {
+      a.classList.remove("active");
+      if (a.getAttribute("href") === `#${current}`) {
+        a.classList.add("active");
+      }
+    });
+  });
+});
+
+
+
+// Hamburger
+
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+  if (navLinks.classList.contains("show")) {
+    navLinks.classList.remove("show");
+    navLinks.classList.add("hide");
+  } else {
+    navLinks.classList.remove("hide");
+    navLinks.classList.add("show");
+  }
+  hamburger.classList.toggle("active");
+});
+
